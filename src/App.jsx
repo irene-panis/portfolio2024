6015728154c369c64234bc507d1d7f9d057433b9
irene <panisirene@gmail.com>
2024-03-16 12:45:21 -0700

    build navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './index.css'
 import { Line } from './components/home/Line'
+import { Navbar } from './components/Navbar'
 
 
 function App() {
 
   return (
-    <>
+    <div className="app-wrapper px-10">
       <span className="bg-black text-2xl">Hi there, I&apos;m Irene.</span>
       <Line/>
-    </>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex gap-[25%]">
+        <div>
+          <li>home</li>
+          <li>projects</li>
+        </div>
+        <div>
+          <li>contact</li>
+          <li>resume</li>
+        </div>
+        <div>
+          <li>light mode</li>
+        </div>
+      </ul>
+    </nav>
+  )
+}
