97732b5f75491a8640fca2290a6e4687b5d76cb8
irene <panisirene@gmail.com>
2024-03-17 20:29:18 -0700

    add accent class to elements w/ accent colors

--- src/components/home/Name.jsx
+++ src/components/home/Name.jsx
@@ -1,6 +1,6 @@
 export const Name = () => {
   return (
-    <div className="home-cont flex flex-col justify-center">
+    <div className="home-cont flex flex-col justify-center accent">
       <h1>Hello there,<br/> my name&apos;s Irene.</h1>
     </div>
   )
