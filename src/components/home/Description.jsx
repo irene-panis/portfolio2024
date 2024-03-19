2b3375b97833ca128af34996e674855ce2e343e3
irene <panisirene@gmail.com>
2024-03-18 19:12:52 -0700

    create header and add media query for homepage

--- src/components/home/Description.jsx
+++ src/components/home/Description.jsx
@@ -1,7 +1,8 @@
 export const Description = () => {
   return (
-    <div className="home-cont">
-      <div>yes</div>
+    <div className="home-cont home-desc flex flex-col">
+      <h2 className="accent dark-mode-accent">Full-stack developer,<br className="hidden"/> software engineer.</h2>
+      <div>Experienced in building and delivering full-stack web applications. Always building cool stuff. When I&apos;m not coding, you can find me playing video games or tuned into a Twitch stream. </div>
     </div>
   )
 }
