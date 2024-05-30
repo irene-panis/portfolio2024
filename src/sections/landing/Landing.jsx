f29b1715e3099968b7bb6700080795f945b591c5
irene <panisirene@gmail.com>
2024-05-29 21:00:54 -0700

    make landing page responsive

--- src/sections/landing/Landing.jsx
+++ src/sections/landing/Landing.jsx
@@ -4,8 +4,8 @@ export const Landing = () => {
     <section id="landing" className="flex flex-col justify-center items-center gap-4 landing">
       <h2>Full-stack Software Engineer</h2>
       <img src="/avatar.png" height="200" width="200"/>
-      <div className="subheaders uppercase text-center px-8">
-        <p>UCSD grad turned bootcamp student turned full-stack developer trying to build cool stuff.</p>
+      <div className="subheaders uppercase lg:flex lg:justify-between text-center px-8 lg:min-w-[45%]">
+        <p className="lg:max-w-[200px] lg:text-left">UCSD grad turned bootcamp student turned full-stack developer trying to build cool stuff.</p>
         <p>EST. SOCAL 2024</p>
       </div>
     </section>
