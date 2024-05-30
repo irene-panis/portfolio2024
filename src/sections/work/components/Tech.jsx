327205a5303fed166d105cbdd26fea4c56832420
irene <panisirene@gmail.com>
2024-05-29 19:48:54 -0700

    link github profile

--- src/sections/work/components/Tech.jsx
+++ src/sections/work/components/Tech.jsx
@@ -4,7 +4,7 @@ export const Tech = () => {
     <div className="h-screen flex flex-col justify-center gap-8 work-intro">
       <p className="uppercase font-bold text-[2em]">Hi, I'm Irene.</p>
       <p>I'm a full-stack software engineer based in Southern California. I love to solve complex problems and I'd love to make a career out of building cool things I and other people can enjoy.</p>
-      <p>Some of my past works can be viewed below and on my GitHub profile.</p>
+      <p>Some of my past works can be viewed below and on my <a href="https://github.com/irene-panis" className="underline">GitHub profile.</a></p>
       <div className="techs flex flex-col gap-2">
         <p>Technologies</p>
         <ul className="tech-list flex flex-wrap gap-2 list-none lowercase">
