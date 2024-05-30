29d7aaba6984cbe195e31a71e8b4f8554303613b
irene <panisirene@gmail.com>
2024-05-29 19:41:03 -0700

    pass project data to project component

--- src/sections/work/Work.jsx
+++ src/sections/work/Work.jsx
@@ -1,11 +1,15 @@
 import { Tech } from "./components/Tech"
 import { Project } from "./components/Project"
+import { projects } from "../../utils/projectData";
 
 export const Work = () => {
   return (
     <section id="work" className="flex flex-col items-center gap-4">
       <Tech/>
-      <Project/>
+      {
+      projects.map((project, index) => (
+        <Project key={index} project={project} index={index + 1}/>
+      ))}
     </section>
   )
 }
--- src/sections/work/components/Project.jsx
+++ src/sections/work/components/Project.jsx
@@ -1,11 +1,18 @@
-export const Project = () => {
+/* eslint-disable react/prop-types */
+export const Project = ( {project, index} ) => {
   return (
     <div className="h-screen w-full flex flex-col gap-8">
-      <div className="project-title flex flex-col justify-center items-center h-1/2">
-        <p>project title!</p>
+      <div className="project-info p-10 flex flex-col gap-2 justify-center h-1/2">
+        <p>0{index}.</p>
+        <p>{project.title}</p>
+        <p>{project.description}</p>
+        <div className="project-links flex gap-2 underline">
+          <a href={project.repo}>GitHub</a>
+          <a href={project.live}>Live</a>
+        </div>
       </div>
       <div className="project-photo h-1/2">
-        <img src="/thinkthieves.png" className="w-full h-full object-cover object-left"/>
+        <img src={project.img} className="w-full h-full object-cover object-left"/>
       </div>
     </div>
   )
