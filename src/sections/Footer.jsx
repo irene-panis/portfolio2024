47f8701887bfb37e9cbc28206007326b7433a870
irene <panisirene@gmail.com>
2024-05-29 22:29:18 -0700

    add lil fade in animation for landing page

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="footer-wrapper flex flex-col justify-evenly items-center text-white gap-5">
-      <span className="text-center">made with 🤍 by <a href="https://github.com/irene-panis/think-thieves" target="_blank" className="underline hover:bg-white hover:text-black duration-200 ease-in-out">irene panis</a>.</span>
+      <span className="text-center">made with 🤍 by <a href="https://github.com/irene-panis/think-thieves" target="_blank" className="underline">irene panis</a>.</span>
     </div>
   )
 }
