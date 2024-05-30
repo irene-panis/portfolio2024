11dea99cab0a9ce01cbd0ed7ec607263d6632d9e
irene <panisirene@gmail.com>
2024-05-29 19:26:01 -0700

    add project data

---
+++ src/utils/projectData.js
@@ -0,0 +1,16 @@
+[
+  {
+    title: 'Think Thieves',
+    description: `Reimagining of the 100 Thieves website with a focus on their esports sector. Web scrapers and API calls are used in tandem to grab the latest information about upcoming matches and who's currently live.`,
+    img: '/thinkthieves.png',
+    repo: 'https://github.com/irene-panis/think-thieves',
+    live: 'https://thinkthieves-c78e2959a576.herokuapp.com/'
+  },
+  {
+    title: 'Jobhunter',
+    description: `Full-stack CRUD application built using the MERN stack that allows users to keep track of jobs they've applied for.`,
+    img: '/jobhunter.png',
+    repo: 'https://github.com/irene-panis/jobhunter',
+    live: 'https://jobhunterapp-345c7e7b566e.herokuapp.com/'
+  }
+]
