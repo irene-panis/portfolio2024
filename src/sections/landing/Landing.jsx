e7d4405706782af9c82fe342fd666080a20cd830
irene <panisirene@gmail.com>
2024-05-29 18:52:19 -0700

    add work intro section

--- src/sections/landing/Landing.jsx
+++ src/sections/landing/Landing.jsx
@@ -1,12 +1,12 @@
 
 export const Landing = () => {
   return (
-    <section id="landing" className="flex flex-col justify-center items-center gap-4 landing main-cont">
+    <section id="landing" className="flex flex-col justify-center items-center gap-4 landing">
       <h2>Full-stack Software Engineer</h2>
       <img src="/avatar.png" height="200" width="200"/>
       <div className="subheaders uppercase text-center px-8">
         <p>UCSD grad turned bootcamp student turned full-stack developer trying to build cool stuff.</p>
-        <p>EST. 2023</p>
+        <p>EST. SOCAL 2024</p>
       </div>
     </section>
   )
--- src/sections/work/Work.jsx
+++ src/sections/work/Work.jsx
@@ -1,13 +1,9 @@
+import { Tech } from "./components/Tech"
 
 export const Work = () => {
   return (
-    <section id="landing" className="flex flex-col justify-center items-center gap-4 main-cont">
-      <h2>Full-stack Software Engineer</h2>
-      <img src="/avatar.png" height="200" width="200"/>
-      <div className="subheaders uppercase text-center px-8">
-        <p>UCSD grad turned bootcamp student turned full-stack developer trying to build cool stuff.</p>
-        <p>EST. 2023</p>
-      </div>
+    <section id="work" className="flex flex-col justify-center items-center gap-4 main-cont work-intro">
+      <Tech/>
     </section>
   )
 }
