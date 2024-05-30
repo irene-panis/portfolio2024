26d6355a06d0313553915063fd8d401529db6c96
irene <panisirene@gmail.com>
2024-05-29 21:05:21 -0700

    tweak spacing and font size on bigger screen sizes

--- src/sections/landing/Landing.jsx
+++ src/sections/landing/Landing.jsx
@@ -2,10 +2,10 @@
 export const Landing = () => {
   return (
     <section id="landing" className="flex flex-col justify-center items-center gap-4 landing">
-      <h2>Full-stack Software Engineer</h2>
+      <h2 className="lg:max-w-[50%]">Full-stack Software Engineer</h2>
       <img src="/avatar.png" height="200" width="200"/>
-      <div className="subheaders uppercase lg:flex lg:justify-between text-center px-8 lg:min-w-[45%]">
-        <p className="lg:max-w-[200px] lg:text-left">UCSD grad turned bootcamp student turned full-stack developer trying to build cool stuff.</p>
+      <div className="subheaders uppercase lg:flex lg:justify-between text-center px-8 lg:min-w-[50%]">
+        <p className="lg:max-w-[400px] lg:text-left">UCSD grad turned bootcamp student turned full-stack developer trying to build cool stuff.</p>
         <p>EST. SOCAL 2024</p>
       </div>
     </section>
