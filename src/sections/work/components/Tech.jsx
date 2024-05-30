2821756f5a1fff00b19a43de37be8fc6d584d299
irene <panisirene@gmail.com>
2024-05-29 19:01:17 -0700

    add tech list to work intro

--- src/sections/work/components/Tech.jsx
+++ src/sections/work/components/Tech.jsx
@@ -5,8 +5,18 @@ export const Tech = () => {
       <p className="uppercase font-bold text-[2em]">Hi, I'm Irene.</p>
       <p>I'm a full-stack software engineer based in Southern California. I love to solve complex problems and I'd love to make a career out of building cool things I and other people can enjoy.</p>
       <p>Some of my past works can be viewed below and on my GitHub profile.</p>
-      <div className="techs">
+      <div className="techs flex flex-col gap-2">
         <p>Technologies</p>
+        <ul className="tech-list flex flex-wrap gap-2 list-none lowercase">
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>React</li>
+          <li>Express.js</li>
+          <li>Node.js</li>
+          <li>SQL</li>
+          <li>NoSQL</li>
+        </ul>
       </div>
     </div>
   )
