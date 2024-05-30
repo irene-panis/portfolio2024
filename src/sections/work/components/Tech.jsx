8c52c31bf256b848768dfb87377f597c4582958b
irene <panisirene@gmail.com>
2024-05-29 20:09:12 -0700

    add a lil more me to my about section

--- src/sections/work/components/Tech.jsx
+++ src/sections/work/components/Tech.jsx
@@ -4,6 +4,7 @@ export const Tech = () => {
     <div className="h-screen flex flex-col justify-center gap-8 work-intro">
       <p className="uppercase font-bold text-[2em]">Hi, I'm Irene.</p>
       <p>I'm a full-stack software engineer based in Southern California. I love to solve complex problems and I'd love to make a career out of building cool things I and other people can enjoy.</p>
+      <p>Outside of coding, I love to game, work out, and try new foods. Ask me about VALORANT esports sometime #100WIN :)</p>
       <p>Some of my past works can be viewed below and on my <a href="https://github.com/irene-panis" className="underline">GitHub profile.</a></p>
       <div className="techs flex flex-col gap-2">
         <p>Technologies</p>
