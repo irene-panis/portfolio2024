1351c8821a778a9c24258ecf9c5d9ed684d229fb
irene <panisirene@gmail.com>
2024-05-29 19:08:32 -0700

    create project component

---
+++ src/sections/work/components/Project.jsx
@@ -0,0 +1,8 @@
+export const Project = () => {
+  return (
+    <div className="h-full flex flex-col gap-8">
+      <div className="project-title"></div>
+      <div className="project-photo"></div>
+    </div>
+  )
+}
